b9e1cceda6d8b738f1193a65b132e539c191267c
Calum-Campbell <campbell.cj@hotmail.co.uk>
2016-01-11 11:57:35 +0000

    basic set up, rendering piece

--- fundamentals/App.js
+++ fundamentals/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
-class App extends React.Component {
-  render(){
-    return <div>Hello</div>
-  }
-}
+var React = require('react');
+var PropTypes = React.PropTypes;
 
-export default App
+
+var Knight = React.createClass({
+  render: function() {
+    return <span>K</span>
+  }
+})
+
+var Square = React.createClass({
+  propTypes: {
+    black : PropTypes.bool
+  },
+
+  render: function() {
+    var black = this.props.black;
+    var fill = black ? 'black' : 'white';
+    return <div style={{ backgroundColor: fill }} />;
+  }
+})
+
+module.exports = Knight;
+// module.exports = Square;
--- fundamentals/main.js
+++ fundamentals/main.js
@@ -1,4 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
+var React = require('react')
+var ReactDOM = require('react-dom');
+// var App = require('./App')
+var App = require('./App')
 ReactDOM.render(<App />, document.getElementById('app'))
+
